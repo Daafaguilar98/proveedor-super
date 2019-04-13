6ca72b2689e9f92e9fefbc9d4d1e5caf63edcd81
Diego Aguilar <daafaguilar98@gmail.com>
2019-04-13 18:02:46 -0500

    new release

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Admin from './Views/Admin';
 class App extends Component {
+  state = {
+    collapsed: false
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Admin />;
   }
 }
 
